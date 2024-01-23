bb5a53ba51f8c87138d8339c3d48d896b62e4400
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2024-01-24 02:30:16 +0530

    made request admin form sectino

---
+++ src/components/admin/RequestAdmin/RequestCard.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+type TProps = {
+    handleAddCategory:(category: string) => void;
+    handleAcceptApproval:() => Promise<void>
+    selectedCategories:string[];
+    handleRemoveCategory:(category: string, idx: number) => void;
+    availableCategories:string[]
+}
+
+const RequestCard = ({handleAddCategory,handleAcceptApproval,selectedCategories,handleRemoveCategory,availableCategories}:TProps) => {
+  return (
+    <div className=" w-full p-2 flex flex-col gap-4 border-[1px] border-gray-400 rounded-md">
+
+        <div className="p-1  flex text-[1.3rem] gap-4">
+          <span className="  p-1">Pin code: <strong>221507</strong></span>
+          <span className="  p-1">Phone: <strong>982520785</strong></span>
+          <span className="  p-1">Seller Name: <strong>Pranjal Kesarwani</strong></span>
+          <span className="  p-1">Shop Name: <strong>Hem Chandra Vastra Bhandar</strong></span>
+        </div>
+        <div className="p-1  flex text-[1.3rem] gap-4">
+          <span className="  p-1">Shop Address: <strong>Sahson, Jhunsi Road, Prayagraj</strong></span>
+          <span className="  p-1">Landmark: <strong>Near gol chauraha</strong></span>
+
+        </div>
+        <hr />
+        <div className="p-1  flex flex-col text-[1.3rem] gap-4 ">
+          <span className="  p-1"> <strong>Selected Categories</strong> </span>
+          <ul className=" flex overflow-x-auto space-x-4 p-4 w-full  ">
+            {
+              selectedCategories.length === 0 ? <><div className="w-full text-5xl text-gray-400">Select atleast 1 category</div></>:(
+                selectedCategories.map((category, idx) => {
+                  return (
+                    <div key={idx} className="border border-gray-300 min-w-[15rem] text-center p-2 rounded cursor-pointer flex justify-between bg-green-200 gap-5"><span className="">{category} </span><span className="   rounded-full hover:bg-red-700 hover:text-white w-[2rem] h-[2rem] flex items-center justify-center" onClick={() => handleRemoveCategory(category, idx)}><i className="fa-solid fa-xmark"></i></span></div>
+  
+                  )
+                })
+              )
+           
+            }
+          </ul>
+
+        </div>
+        <hr />
+        <div className=" w-full ">
+          <h1 className="font-bold text-xl w-full">Available categories:</h1>
+          <ul className=" flex overflow-x-auto space-x-4 p-4 w-full  ">
+            {
+              availableCategories.map((category, idx) => {
+                return (
+                  <div key={idx} className="border border-gray-300 min-w-[15rem] text-center p-2 rounded cursor-pointer" onClick={() => handleAddCategory(category)}>{category}</div>
+
+                )
+              })
+            }
+
+          </ul>
+        </div>
+        <hr />
+        <div className="w-full flex justify-end">
+
+        <button className="bg-green-600 text-white text-2xl py-2 w-[15rem] rounded-lg mr-8 mb-3" onClick={handleAcceptApproval}>Approve Request</button>
+        </div>
+
+      </div>
+  )
+}
+
+export default RequestCard
--- src/components/admin/RequestsAdmin.tsx
+++ src/components/admin/RequestsAdmin.tsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { BASE_URL } from "../../Url";
+import RequestCard from "./RequestAdmin/RequestCard";
 
 const RequestsAdmin = () => {
-  return (
-    <div className="w-full h-full bg-white rounded-lg flex items-center justify-center">
 
-    <h1>List of all requests for shop opening admin dashboard</h1>
-</div>
+  const marketAreaPinCodes = ['221507', '221508', '221509', '221510'];
+  const availableCategories = ['Cloths', 'Gifts', 'Goggles & Spectacles', 'Shoes', 'Matty'];
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+
+  const handleAddCategory = (category: string) => {
+    for (let i = 0; i < selectedCategories.length; i++) {
+      if (selectedCategories[i] === category) {
+        alert('Category already added');
+        return
+      }
+    }
+
+    setSelectedCategories((prev) => {
+
+      return [...prev, category] as string[]
+    })
+  }
+
+  const handleRemoveCategory = (category: string, idx: number) => {
+
+    console.log(category, idx)
+    selectedCategories.splice(idx, 1);
+    setSelectedCategories((prev) => {
+      return [...prev]
+    });
+
+  }
+
+  const handleAccpetApproval = async () => {
+
+    if (selectedCategories.length === 0) {
+      alert('Select atleast one category!');
+      return;
+    }
+    try {
+      const res = await axios.put(`${BASE_URL}/auth/seller-request-approval`);
+
+    } catch (error) {
+
+    }
+  }
+
+  return (
+    <div className="w-full h-full bg-white rounded-lg flex flex-col p-3  gap-3">
+      <h1 className="text-4xl font-semibold text-gray-600">All Requests</h1>
+      <hr />
+      <div className=" w-full p-2  ">
+        <ul className=" flex gap-2 justify-evenly text-xl">
+          {
+            marketAreaPinCodes.map((pin, idx) => {
+              return (
+                <li key={idx} className="border-[1px] border-black rounded-full px-2 py-1 min-w-[12rem] text-center">{pin}</li>
+
+              )
+            })
+          }
+
+        </ul>
+      </div>
+      <hr />
+      {/* <div className=" w-full p-2 text-xl flex flex-col gap-2 mt-2 ">
+        <h1 className=" ml-4">Available shop categories for 221507</h1>
+        <ul className=" flex overflow-x-auto space-x-4 p-4">
+          {
+            availableCategories.map((category, idx) => {
+              return (
+                <div key={idx} className="border border-gray-300 min-w-[15rem] text-center p-2 rounded">{category}</div>
+
+              )
+            })
+          }
+
+
+
+        </ul>
+
+      </div> */}
+
+      <hr />
+
+      <div className="w-full h-full overflow-y-auto flex flex-col space-y-5">
+        <RequestCard
+          handleAddCategory={handleAddCategory}
+          handleAcceptApproval={handleAccpetApproval}
+          selectedCategories={selectedCategories}
+          handleRemoveCategory={handleRemoveCategory}
+          availableCategories={availableCategories}
+        />
+     
+      </div>
+
+
+
+
+
+    </div>
   )
 }
 
