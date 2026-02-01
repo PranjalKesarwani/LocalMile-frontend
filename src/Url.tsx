d7188ad560b89ef01582557b936a48e6df9191ae
Pranjal Kesarwani <pranjalkesarwani2000@gmail.com>
2026-02-01 18:38:00 +0530

    working

--- src/Url.tsx
+++ src/Url.tsx
@@ -1,6 +1,6 @@
 // export const BASE_URL = "https://localmile-backend.onrender.com/api/v1";
-export const BASE_URL = "http://localhost:8080/api/v1";
-// export const BASE_URL = "https://api.unlocl.com/api/v1";
+// export const BASE_URL = "http://localhost:8080/api/v1";
+export const BASE_URL = "https://api.unlocl.com/api/v1";
 
 export const post_config = {
   headers: {
